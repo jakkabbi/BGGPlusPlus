630d5264e39890ceb2cd8f94caecf9c73f6743c6
Jack McClure <jmcclure@nexient.com>
2019-10-19 12:52:58 -0400

    Add Board Game Geek api class

---
+++ BGGPlusPlus/BGGPlusPlus.Domain/BGGApiService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGGPlusPlus.Domain
+{
+    public class BGGApiService
+    {
+        public async Task<string> GetBGGApi()
+        {
+            List<int> tempList = new List<int>();
+            for (int i = 100000; i < 101160; i++)
+                tempList.Add(i);
+
+            var endpoint = $"https://www.boardgamegeek.com/xmlapi2/thing?id={string.Join(",", tempList)}&stats=1";
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
+
+                using (HttpResponseMessage response = await httpClient.GetAsync(endpoint))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        var result = await content.ReadAsByteArrayAsync();
+                        var data = Encoding.UTF8.GetString(result, 0, result.Length);
+                        return data;
+                    }
+                }
+            }
+        }
+    }
+}
