0230f6a6e88779fde486f1c6eac4b2e0854effd3
Jack McClure <jmcclure@nexient.com>
2019-10-19 16:28:06 -0400

    Deserialize data from bgg into poco

--- BGGPlusPlus/BGGPlusPlus.Domain/BGGApiService.cs
+++ BGGPlusPlus/BGGPlusPlus.Domain/BGGApiService.cs
@@ -4,6 +4,11 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.Linq;
+using System.IO;
+using System.Xml;
 
 namespace BGGPlusPlus.Domain
 {
@@ -11,8 +16,10 @@ namespace BGGPlusPlus.Domain
     {
         public async Task<string> GetBGGApi()
         {
+            XmlSerializer serializer = new XmlSerializer(typeof(Items));
+
             List<int> tempList = new List<int>();
-            for (int i = 100000; i < 101160; i++)
+            for (int i = 1; i < 2; i++)
                 tempList.Add(i);
 
             var endpoint = $"https://www.boardgamegeek.com/xmlapi2/thing?id={string.Join(",", tempList)}&stats=1";
@@ -28,7 +35,13 @@ namespace BGGPlusPlus.Domain
                     {
                         var result = await content.ReadAsByteArrayAsync();
                         var data = Encoding.UTF8.GetString(result, 0, result.Length);
-                        return data;
+
+                        using (TextReader reader = new StringReader(data))
+                        {
+                            var test = (Items)serializer.Deserialize(reader);
+
+                            return "success";
+                        }
                     }
                 }
             }
