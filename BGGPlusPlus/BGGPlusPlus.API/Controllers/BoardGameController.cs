9a07f41613432540dba238fedb86bab295a175fe
Jack McClure <jmcclure@nexient.com>
2019-10-19 17:34:41 -0400

    Added primary controller with 5 endpoints, removed sample controller

---
+++ BGGPlusPlus/BGGPlusPlus.API/Controllers/BoardGameController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BGGPlusPlus.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardGameController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetArtists()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDesigners()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPublishers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SearchBoardGames()
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
